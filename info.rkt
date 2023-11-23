4d3cfad04935d3a5afb848ef02a23f535283e35e
Nikhil Marathe <me@nikhilism.com>
2023-11-22 16:18:57 -0800

    lp2 dependency and other updates to info.rkt

--- info.rkt
+++ info.rkt
@@ -1,9 +1,9 @@
 #lang info
 (define collection "git-catter")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "scribble-lp2-manual"))
 (define scribblings '(("scribblings/git-catter.scrbl" ())))
-(define pkg-desc "Description Here")
-(define version "0.0")
-(define pkg-authors '(nikhil))
+(define pkg-desc "A tutorial on using Racket concurrency primitives for a practical program")
+(define version "0.1")
+(define pkg-authors '(racket-packages@me.nikhilism.com))
 (define license '(MIT))
